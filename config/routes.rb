990287628e1f24eb32ddf874fb387ea679e0d019
“h-reader” <super.black.jack777@gmail.com>
2017-11-17 23:24:15 +0900

    microposts ルーティングを /api 配下に変更。

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
 
-  resources :microposts
 
   namespace :api do
     mount_devise_token_auth_for 'User', at: 'auth', controllers: {
       registrations: 'api/auth/registrations'
     }
+  end
 
-
+  scope :api do
+    resources :microposts, controllers: :microposts
   end
 
 end
